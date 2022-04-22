518e8666f4f344fb3328f2d8fdcec7c069a69576
Caner <caner.isik@assistbox.io>
2022-04-22 18:21:49 +0300

    updated to be compatible with assistbox sdk v4.4.1

--- ios/ReactNativeAssistbox/ReactNativeAssistbox.m
+++ ios/ReactNativeAssistbox/ReactNativeAssistbox.m
@@ -19,63 +19,168 @@ RCT_EXPORT_MODULE()
 	return NO;
 }
 
-RCT_EXPORT_METHOD(goToAssistbox:(NSString *)token
+RCT_EXPORT_METHOD(initVideoCallWithToken:(NSString *)token
 				  mobileServiceEndpoint:(NSString *)mobileServiceEndpoint
-				  mobileStorageEndpoint:(NSString *)mobileStorageEndpoint
 				  splashScreenResourceName:(NSString *)splashScreenResourceName
+				  regularNotificationToken: (NSString *)regularNotificationToken
 				  successCallback: (RCTResponseSenderBlock)successCallback
 				  errorCallback: (RCTResponseSenderBlock)errorCallback {
-
+	
 #if TARGET_IPHONE_SIMULATOR
 	errorCallback(@[@"Assistbox SDK does not support iOS Simulators"]);
 	return;
 #endif
-
+	
+	if (token == nil || [token length] == 0){
+		errorCallback(@[@"Token is required"]);
+		return;
+	}
+	if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
+		errorCallback(@[@"Mobile Service Endpoint is required"]);
+		return;
+	}
+	
 	dispatch_async(dispatch_get_main_queue(), ^{
-		NSBundle *frameworkBundle = [NSBundle bundleForClass:[AssistBoxViewController class]];
-		UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"AssistBox" bundle:frameworkBundle];
-		AssistBoxViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"assistBoxViewController"];
-		vc.delegate = self;
-		
-		if (token == nil || [token length] == 0){
-			errorCallback(@[@"Token is required"]);
-			return;
-		}
-		if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
-			errorCallback(@[@"Mobile Service Endpoint is required"]);
-			return;
-		}
-		if (mobileStorageEndpoint == nil || [mobileStorageEndpoint length] == 0){
-			errorCallback(@[@"Mobile Storage Endpoint is required"]);
-			return;
-		}
-		
-		vc.token = token;
-		vc.mobileServiceEndpoint = mobileServiceEndpoint;
-		vc.mobileStorageEndpoint = mobileStorageEndpoint;
-		vc.splashScreenResourceName = splashScreenResourceName;
-		vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
-		
-		UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-		
-		UIViewController* topMostViewController = [self getTopMostViewController];
-		
-		if(topMostViewController.navigationController){
-			vc.isMainAppNavigationBarHidden = topMostViewController.navigationController.isNavigationBarHidden;
-			if(!topMostViewController.navigationController.isNavigationBarHidden){
-				topMostViewController.navigationController.navigationBar.hidden = true;
-			}
-			vc.hasNavigationStack = YES;
-			[topMostViewController.navigationController pushViewController:vc animated:NO];
-		} else {
-			UIViewController* navController = (UIViewController*)window.rootViewController;
-			vc.hasNavigationStack = NO;
-			[navController presentViewController:vc animated:NO completion:nil];
-		}
+		UIViewController* vc = [self getViewController];
+		NSLog(@"viewController: %@", vc);
+		[[Assistbox shared] initVideoCallWithTokenWithViewController:vc token:token mobileServiceEndpoint:mobileServiceEndpoint splashScreenResourceName:splashScreenResourceName];
 		successCallback(@[@"Opening Assistbox SDK"]);
-		
 	});
 });
+
+RCT_EXPORT_METHOD(initVideoCallWithAccessKey:(NSString *)accessKey
+				  mobileServiceEndpoint:(NSString *)mobileServiceEndpoint
+				  splashScreenResourceName:(NSString *)splashScreenResourceName
+				  regularNotificationToken: (NSString *)regularNotificationToken
+				  successCallback: (RCTResponseSenderBlock)successCallback
+				  errorCallback: (RCTResponseSenderBlock)errorCallback {
+	
+#if TARGET_IPHONE_SIMULATOR
+	errorCallback(@[@"Assistbox SDK does not support iOS Simulators"]);
+	return;
+#endif
+	
+	if (accessKey == nil || [accessKey length] == 0){
+		errorCallback(@[@"Access Key is required"]);
+		return;
+	}
+	if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
+		errorCallback(@[@"Mobile Service Endpoint is required"]);
+		return;
+	}
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		UIViewController* vc = [self getViewController];
+		[[Assistbox shared] initVideoCallWithAccessKeyWithViewController:vc accessKey:accessKey mobileServiceEndpoint:mobileServiceEndpoint splashScreenResourceName:splashScreenResourceName regularNotificationToken:regularNotificationToken redirectToMainApplication:YES];
+		successCallback(@[@"Opening Assistbox SDK"]);
+	});
+});
+
+RCT_EXPORT_METHOD(initC2CModuleAsAgent:(NSString *)mobileServiceEndpoint
+				  voipNotificationToken:(NSString *)voipNotificationToken
+				  splashScreenResourceName:(NSString *)splashScreenResourceName
+				  regularNotificationToken: (NSString *)regularNotificationToken
+				  successCallback: (RCTResponseSenderBlock)successCallback
+				  errorCallback: (RCTResponseSenderBlock)errorCallback {
+	
+#if TARGET_IPHONE_SIMULATOR
+	errorCallback(@[@"Assistbox SDK does not support iOS Simulators"]);
+	return;
+#endif
+	
+	if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
+		errorCallback(@[@"Mobile Service Endpoint is required"]);
+		return;
+	}
+	if (voipNotificationToken == nil || [voipNotificationToken length] == 0){
+		errorCallback(@[@"VoIP Notification Token is required"]);
+		return;
+	}
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		UIViewController* vc = [self getViewController];
+		[Assistbox.shared initC2CModuleAsAgentWithViewController:vc mobileServiceEndpoint:mobileServiceEndpoint voipNotificationToken:voipNotificationToken splashScreenResourceName:splashScreenResourceName regularNotificationToken:regularNotificationToken redirectToMainApplication:YES];
+		successCallback(@[@"Opening Assistbox SDK"]);
+	});
+});
+
+RCT_EXPORT_METHOD(initC2CModuleAsClientWithApiKey:(NSString *)apiKey
+				  queueCode:(NSString *)queueCode
+				  mobileServiceEndpoint:(NSString *)mobileServiceEndpoint
+				  firstName:(NSString *)firstName
+				  lastName:(NSString *)lastName
+				  email:(NSString *)email
+				  phone:(NSString *)phone
+				  productName:(NSString *)productName
+				  language:(NSString *)language
+				  splashScreenResourceName:(NSString *)splashScreenResourceName
+				  regularNotificationToken: (NSString *)regularNotificationToken
+				  successCallback: (RCTResponseSenderBlock)successCallback
+				  errorCallback: (RCTResponseSenderBlock)errorCallback {
+	
+#if TARGET_IPHONE_SIMULATOR
+	errorCallback(@[@"Assistbox SDK does not support iOS Simulators"]);
+	return;
+#endif
+	
+	if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
+		errorCallback(@[@"Mobile Service Endpoint is required"]);
+		return;
+	}
+	if (apiKey == nil || [apiKey length] == 0){
+		errorCallback(@[@"Api Key is required"]);
+		return;
+	}
+	if (queueCode == nil || [queueCode length] == 0){
+		errorCallback(@[@"Queue Code is required"]);
+		return;
+	}
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		UIViewController* vc = [self getViewController];
+		[Assistbox.shared initC2CModuleAsClientWithApiKeyWithViewController:vc apiKey:apiKey queueCode:queueCode mobileServiceEndpoint:mobileServiceEndpoint firstName:firstName lastName:lastName email:email phone:phone productName:productName language:language splashScreenResourceName:splashScreenResourceName redirectToMainApplication:YES];
+		successCallback(@[@"Opening Assistbox SDK"]);
+	});
+});
+
+RCT_EXPORT_METHOD(initC2CModuleAsClientWithToken:(NSString *)token
+				  mobileServiceEndpoint:(NSString *)mobileServiceEndpoint
+				  splashScreenResourceName:(NSString *)splashScreenResourceName
+				  regularNotificationToken: (NSString *)regularNotificationToken
+				  successCallback: (RCTResponseSenderBlock)successCallback
+				  errorCallback: (RCTResponseSenderBlock)errorCallback {
+	
+#if TARGET_IPHONE_SIMULATOR
+	errorCallback(@[@"Assistbox SDK does not support iOS Simulators"]);
+	return;
+#endif
+	
+	if (mobileServiceEndpoint == nil || [mobileServiceEndpoint length] == 0){
+		errorCallback(@[@"Mobile Service Endpoint is required"]);
+		return;
+	}
+	if (token == nil || [token length] == 0){
+		errorCallback(@[@"Token is required"]);
+		return;
+	}
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		UIViewController* vc = [self getViewController];
+		[Assistbox.shared initC2CModuleAsClientWithTokenWithViewController:vc token:token mobileServiceEndpoint:mobileServiceEndpoint splashScreenResourceName:splashScreenResourceName redirectToMainApplication:YES];
+		successCallback(@[@"Opening Assistbox SDK"]);
+	});
+});
+
+- (UIViewController*) getViewController {
+	UIViewController* topMostViewController = [self getTopMostViewController];
+	if(topMostViewController.navigationController){
+		return topMostViewController;
+	} else {
+		UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+		UIViewController* navController = (UIViewController*)window.rootViewController;
+		return navController;
+	}
+}
 
 - (UIViewController*) getTopMostViewController {
 	UIViewController *presentingViewController = [[[UIApplication sharedApplication] delegate] window].rootViewController;
@@ -83,10 +188,6 @@ RCT_EXPORT_METHOD(goToAssistbox:(NSString *)token
 		presentingViewController = presentingViewController.presentedViewController;
 	}
 	return presentingViewController;
-}
-
-- (void)AssistBoxViewWillDisappear:(BOOL)animated {
-	[self sendAssistboxRedirectEvent];
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -105,6 +206,10 @@ RCT_EXPORT_METHOD(goToAssistbox:(NSString *)token
 	if(hasListeners){
 		[self sendEventWithName:@"onAssistboxRedirectPrivate" body:@{}];
 	}
+}
+
+- (void)AssistboxViewWillDisappearWithAnimated:(BOOL)animated {
+	[self sendAssistboxRedirectEvent];
 }
 
 @end
